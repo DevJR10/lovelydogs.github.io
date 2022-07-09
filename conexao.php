d28da3ed2999929ebac103c7245a58cac45a9fa0
Joao Rodrigo <jralbertini27@gmail.com>
2022-07-09 01:21:58 -0300

    raiva do crl

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor="localhost";
-$banco="tcc";
-$usuario="root";
-$senha="";
-$conexao=mysqli_connect($servidor,$usuario,$senha,$banco) or die("Erro");
-
-
-?>
--- js/script.js
+++ js/script.js
@@ -51,3 +51,46 @@ linksInternos.forEach((link) => {
 
 //    botao.addEventListener('click', abrirMenu)
 
+
+
+const sectionQuemSomos = document.querySelectorAll('.somos-scroll')
+const windowMetade = window.innerHeight * 0.85
+
+function scroll(){
+    sectionQuemSomos.forEach((item) => {
+        const alturaSectionSomos = item.getBoundingClientRect().top
+        const somaSectionSomos = (alturaSectionSomos - windowMetade) <= 0
+        if (somaSectionSomos){
+            item.classList.add('ativo-scroll')
+        }
+    })
+}
+window.addEventListener('scroll', scroll)
+
+const sectionAdotar = document.querySelectorAll('.adotar-scroll')
+const windowAdotar = window.innerHeight * 0.9
+
+function scrollAdotar(){
+   sectionAdotar.forEach((item) => {
+        const alturaSectionSomos = item.getBoundingClientRect().top
+        const somaSectionSomos = (alturaSectionSomos - windowAdotar) <= 0
+        if (somaSectionSomos){
+            item.classList.add('ativo-adotar')
+        }
+    })
+}
+window.addEventListener('scroll', scrollAdotar)
+
+const sectionPqFazemos = document.querySelectorAll('.porque-scroll')
+const windowFazemos = window.innerHeight * 0.9
+
+function scrollPorque(){
+   sectionPqFazemos.forEach((item) => {
+        const alturaSectionSomos = item.getBoundingClientRect().top
+        const somaSectionSomos = (alturaSectionSomos - windowFazemos) <= 0
+        if (somaSectionSomos){
+            item.classList.add('ativo-porque')
+        } 
+    })
+}
+window.addEventListener('scroll', scrollPorque)
